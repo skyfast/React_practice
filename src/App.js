6c4ff69f177fce70cde633a350f9866fc547a1c3
Skylar <theskyfast@yahoo.com>
2021-08-31 21:02:55 -0700

    Added reminder status to all list items and toggle ablity with double click

--- src/App.js
+++ src/App.js
@@ -30,11 +30,17 @@ const deleteTask = (id) => {
   setTasks(tasks.filter( (task) => task.id !== id))
 }
 
+// toggle reminder
+const toggleReminder = (id) =>{
+  setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder}: task))
+}
+
   return (
     <div className="container">
       <Header title="Welcome To Sky's app" >
       </Header>
-      {tasks.length > 0 ?<Tasks tasks={tasks} onDelete={deleteTask}/> : "No Tasks to show!"}
+      {tasks.length > 0 ?<Tasks tasks={tasks} onDelete={deleteTask}
+      onToggle={toggleReminder}/> : "No Tasks to show!"}
     </div>
   );
 }
