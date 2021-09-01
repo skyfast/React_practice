5e50b698d89452fa3cb1f7b37535673088169ed5
Skylar <SkylarS@oslc.org>
2021-08-31 20:13:25 -0700

    Added Tasks and started working with state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-
+import { useState } from 'react';
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'School meeting',
+        day: 'Feb 6th at 3:30pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Go to store',
+        day: 'Feb 5th at 2:30pm',
+        reminder: false,
+    }
+])
   return (
     <div className="container">
-      <Header title="Welcome To the Internet" >
-
+      <Header title="Welcome To Sky's app" >
       </Header>
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
